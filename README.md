ecf3e430538d64ed7102c1570a65653064e5bf12
Nathan Pereira de Souza Lima <l.nathan@aluno.ifsp.edu.br>
2026-03-24 14:52:30 -0300

    adicionando projeto

---
+++ README.md
@@ -0,0 +1,29 @@
+# Tópicos Importantes
+
+- Qual a linguagem de programação utilizada na disciplina?
+- O que é o Node?
+- O que é Framework e qual a diferença para uma biblioteca?
+- O que é o NPM?
+- O que é React?
+- O que é o React Native?
+- O que é o Expo?
+- Como estilizar um componente no React Native?
+- O estilo CSS in JS no React Native é igual ao CSS Web? Tenho todas as propriedades?
+- Em quais pacotes encontro um componente de Imagem? Qual melhor para usarmos atualmente?
+- O que são propriedades de componentes?
+- O que são Hooks?
+
+Extra
+
+- Revisão de variáveis e objetos
+    - desestruturação de objetos
+
+## Comandos Uteis
+
+// Como instalar o React Native com Expo?
+
+npx create-expo-app@latest --template
+
+### Expo
+
+
---
+++ src/components/FormSignUp.jsx
@@ -0,0 +1,89 @@
+import { View, Text, StyleSheet, Button } from 'react-native'
+import { TextInput } from 'react-native-web'
+import { useState } from 'react'
+
+export default function FormSignUp() {
+
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [pass, setPass] = useState("")
+  const [avatar, setAvatar] = useState("")
+
+
+  const handleSubmit = async () => {
+    console.log({name, email, pass, avatar})
+    const response   = await fetch("http://localhost:3000/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({name, email, pass, avatar})
+      })
+      if(response.ok){
+        console.log("Usuário cadastrado com sucesso!")
+        const data = await response.json()
+        console.log(data)
+      } else {
+        console.log("Erro ao cadastrar usuário")
+      }
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Cadastro</Text>
+      <View style={styles.form}>
+        <TextInput 
+            style={styles.inputs} 
+            placeholder="Nome"
+            value={name}
+            onChangeText={setName}
+        />
+        <TextInput 
+            style={styles.inputs} 
+            placeholder="Email"
+            value={email}
+            onChangeText={setEmail}
+        />
+        <TextInput 
+            style={styles.inputs} 
+            placeholder="Senha" 
+            secureTextEntry
+            value={pass}
+            onChangeText={setPass}
+        />
+        <TextInput 
+            style={styles.inputs} 
+            placeholder="Avatar" 
+            secureTextEntry
+            value={avatar}
+            onChangeText={setAvatar}
+        />
+        <Button title="Cadastrar" onPress={handleSubmit} />
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f7f7f7',
+    alignItems: 'center'
+  },
+  form:{
+    width: "90%",
+    gap: 10
+  },
+  inputs:{
+    borderWidth: 1,
+    borderColor: "#d1d1d1",
+    borderStyle: "solid",
+    borderRadius: 6,
+    padding: 8
+  },
+    title: {
+    fontSize: 18,
+    fontWeight: "700",
+    marginBottom: 20
+  }
+})
